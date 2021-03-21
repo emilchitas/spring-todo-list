fa4afba7e327ebbe906387d947da0bd2991ee583
Emil <emil.chitas@gmail.com>
2021-03-21 16:11:47 +0100

    ToDo item class

---
+++ src/main/java/org/learn/model/ToDoItem.java
@@ -0,0 +1,31 @@
+package org.learn.model;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+//https://projectlombok.org/features/Data
+@Data
+@EqualsAndHashCode(of = "id")
+public class ToDoItem {
+    private int id;
+    private String title;
+    private String details;
+    private LocalDate deadline;
+
+    public ToDoItem(String title, String details, LocalDate deadline) {
+        this.title = title;
+        this.details = details;
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDoItem{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", details='" + details + '\'' +
+                ", deadline=" + deadline +
+                '}';
+    }
+}
